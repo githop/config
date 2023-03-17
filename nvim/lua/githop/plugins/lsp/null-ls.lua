e891ec8e0b01c37c5561600818ad8a2adfff7f83
Tom Hopkins <tom@githop.com>
2023-03-17 17:29:15 -0600

    add eslint and gitsigns code actions

--- nvim/lua/githop/plugins/lsp/null-ls.lua
+++ nvim/lua/githop/plugins/lsp/null-ls.lua
@@ -7,6 +7,7 @@ end
 -- for conciseness
 local formatting = null_ls.builtins.formatting -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+local code_actions = null_ls.builtins.code_actions -- to setup code actions
 
 -- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -15,12 +16,12 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
 	-- setup formatters & linters
 	sources = {
-		--  to disable file types use
-		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 		formatting.prettier, -- js/ts formatter
 		formatting.stylua, -- lua formatter
 		diagnostics.eslint_d,
 		diagnostics.stylelint,
+		code_actions.gitsigns,
+		code_actions.eslint_d,
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
