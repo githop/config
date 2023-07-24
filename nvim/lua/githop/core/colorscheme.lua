1bb3a4233df665fa1dd62fe28ca50fe4db057c9d
githop <tom@githop.com>
2023-07-23 19:47:31 -0600

    migrate config to kickstart

--- nvim/lua/githop/core/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = "kanagawa"
-
-local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status then
-	print("Colorscheme not found")
-	return
-end
--- nvim/lua/githop/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
-local status, lualine = pcall(require, "lualine")
-if not status then
-	return
-end
-
--- get lualine nightfly theme
-
-lualine.setup()
