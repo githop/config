5ffbdb8d4791cba1e626693df5638035719a6f2f
Tom Hopkins <tom@githop.com>
2024-03-06 23:39:21 -0700

    nvim alpha

--- nvim/init.lua
+++ nvim/init.lua
@@ -563,6 +563,42 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'goolord/alpha-nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
+
+      local header = [[
+ ██████╗ ██╗████████╗██╗  ██╗ ██████╗ ██████╗ 
+██╔════╝ ██║╚══██╔══╝██║  ██║██╔═══██╗██╔══██╗
+██║  ███╗██║   ██║   ███████║██║   ██║██████╔╝
+██║   ██║██║   ██║   ██╔══██║██║   ██║██╔═══╝ 
+╚██████╔╝██║   ██║   ██║  ██║╚██████╔╝██║     
+ ╚═════╝ ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝     
+      ]]
+      dashboard.section.header.val = vim.split(header, '\n')
+      dashboard.section.buttons.val = {
+        dashboard.button('f', '  find file', '<cmd>FzfLua files<CR>'),
+        dashboard.button('r', '  recent', '<cmd>FzfLua oldfiles<CR>'),
+        dashboard.button('g', '  search', '<cmd>FzfLua live_grep_glob<CR>'),
+        dashboard.button('q', '󰅚  Quit NVIM', ':qa<CR>'),
+      }
+      local handle = io.popen 'fortune'
+      local fortune = handle:read '*a'
+      handle:close()
+      dashboard.section.footer.val = fortune
+
+      dashboard.config.opts.noautocmd = true
+
+      vim.cmd [[autocmd User AlphaReady echo 'ready']]
+
+      alpha.setup(dashboard.config)
+    end,
+  },
+
   -- require 'kickstart.plugins.debug',
 }, {})
 --
