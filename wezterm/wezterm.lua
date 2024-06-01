5cca0cfa9fae5207b2e60700f638cf1e54931972
githop <tom@githop.com>
2024-05-31 18:22:56 -0600

    check in

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.default_cursor_style = "BlinkingBar"
+config.force_reverse_video_cursor = true
+
+config.color_scheme = "Kanagawa Dragon"
+
+config.font = wezterm.font("IosevkaTerm Nerd Font Mono", { weight = "Medium" })
+
+config.font_size = 20.5
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+return config
---
+++ zsh/scripts/fnm.sh
@@ -0,0 +1,3 @@
+  # fnm
+export PATH="/Users/githop/Library/Application Support/fnm:$PATH"
+eval "$(fnm env)"
