8307824693bf73da4f75fc817630590957d0465d
githop <tom@githop.com>
2025-01-09 19:41:30 -0700

    temp comment out clipboard stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -121,6 +121,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- [[linux clipboard ssh]]
+-- vim.g.clipboard = {
+--   name = 'OSC 52',
+--   copy = {
+--     ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+--     ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+--   },
+--   paste = {
+--     ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+--     ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+--   },
+-- }
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
