cfa9a7a3b995eab56a8e4bd367cf43af0d9c244f
githop <tom@githop.com>
2025-12-29 16:40:47 -0700

    cuda

--- zsh/scripts/cuda.sh
+++ zsh/scripts/cuda.sh
@@ -1,5 +1,5 @@
-export PATH=/usr/local/cuda-12.1/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-12.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 # Any additional CUDA-related commands you might have...
 
 
